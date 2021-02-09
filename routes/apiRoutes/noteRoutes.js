7377733737730c59e18099178dec2d4f8c78331e
daniela <daniela.acuna93@gmail.com>
2021-02-08 23:51:50 -0600

    install packages

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+const note = require("../../data/notes.json")
+
+router.get('/notes', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+
+module.exports = router; 
